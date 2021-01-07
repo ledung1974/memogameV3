801ac690ad5e405053c17a7ca160b3bdf46244b3
ledung1974 <ledung1974@gmail.com>
2021-01-07 00:26:33 -0600

    Check running on Mobile devices

--- src/pages/StartMemo.js
+++ src/pages/StartMemo.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import '../css/startpage.css';
 import '../css/forms.css';
-import {logo} from '../contexts/GameProvider';
+import { logo } from '../contexts/GameProvider';
 import Login from '../forms/Login';
 import Signup from '../forms/Signup';
 import LevelMemo from '../components/LevelMemo.jsx'
@@ -28,6 +28,13 @@ export default function StartMemo(props) {
     setIsShowResetPassword(false);
   }
 
+  //Checking the Webapp is running on Mobile or not 
+  let onMobile = "";
+  let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  if (isMobile) {
+    onMobile = "MemoGame is running on your Mobile device. Please set the screen's orientation in landscape after login";
+  }
+
   return (
     <div className="start-container">
       <header className="start-header">
@@ -39,13 +46,17 @@ export default function StartMemo(props) {
           : isShowResetPassword ? <ResetPassword hideResetPassword={hideResetPassword} />
             : <>
               <LevelMemo />
-              <Login  showSignUp={showSignUp} 
-                      showResetPassword={showResetPassword}
+              <Login showSignUp={showSignUp}
+                showResetPassword={showResetPassword}
               />
             </>
         }
       </div>
-      <footer><p>MemoGame by Le Dung &#169; 2020</p></footer>
+
+      <footer>
+        <p>{onMobile}</p>
+        <p>MemoGame by Le Dung &#169; 2020</p>
+      </footer>
     </div>
   );
 }
